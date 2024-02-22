2c20ad3167c48a86abba11347c43c7f823192dfa
Chanaka-Prasanna <chanakapinfo@gmail.com>
2024-02-22 11:47:47 +0530

    Last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Authenticated, Refine } from "@refinedev/core";
 import {
-  ThemedLayoutV2,
   useNotificationProvider,
   ErrorComponent,
   RefineThemes,
@@ -13,19 +12,26 @@ import routerBindings, {
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { AntdInferencer } from "@refinedev/inferencer/antd";
 import { dataProvider } from "./rest-data-provider";
-
+import { App as AntdApp } from "antd";
 import { ConfigProvider } from "antd";
 import "@refinedev/antd/dist/reset.css";
-import { BlogPostList } from "./pages/blog-posts/blogpost-list";
-import { BlogPostShow } from "./pages/blog-posts/show-blog";
-import { BlogPostEdit } from "./pages/blog-posts/edit-blog";
-import { BlogPostCreate } from "./pages/blog-posts/create-blog";
+import { BlogPostList } from "./pages/Admin/blog-posts/blogpost-list";
+import { BlogPostShow } from "./pages/Admin/blog-posts/show-blog";
+import { BlogPostEdit } from "./pages/Admin/blog-posts/edit-blog";
+import { BlogPostCreate } from "./pages/Admin/blog-posts/create-blog";
 import authProvider from "./authProvider";
 
 //  to test
 import axios from "axios";
 import createAuthRefreshInterceptor from "axios-auth-refresh";
 import { AuthPage } from "./components/auth";
+import { DevtoolsProvider } from "@refinedev/devtools";
+import { ThemedLayoutV2 } from "./components/admin/layout";
+import { ThemedHeaderV2 } from "./components/admin/layout/header";
+import { ThemedSiderV2 } from "./components/admin/layout/sider";
+import { ThemedTitleV2 } from "./components/admin/layout/title";
+import HomePage from "./pages/Portfoio";
+import NavBar from "./components/portfolio/nav-bar";
 const axiosInstance = axios.create();
 
 // Function that will be called to refresh authorization
@@ -48,73 +54,86 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <ConfigProvider theme={RefineThemes.Magenta}>
-        <Refine
-          routerProvider={routerBindings}
-          authProvider={authProvider}
-          dataProvider={dataProvider(
-            "https://api.fake-rest.refine.dev",
-            axiosInstance
-          )}
-          notificationProvider={useNotificationProvider}
-          resources={[
-            {
-              name: "blog_posts",
-              list: "/blog-posts",
-              show: "/blog-posts/show/:id",
-              create: "/blog-posts/create",
-              edit: "/blog-posts/edit/:id",
-              // this meta property enable delete button on both edit page and show page
-              meta: {
-                canDelete: true,
-              },
-            },
-          ]}
-          options={{
-            syncWithLocation: true,
-            warnWhenUnsavedChanges: true,
-          }}
-        >
-          <Routes>
-            <Route
-              element={
-                <ThemedLayoutV2>
-                  <Outlet />
-                </ThemedLayoutV2>
-              }
+        <AntdApp>
+          <DevtoolsProvider>
+            <Refine
+              routerProvider={routerBindings}
+              authProvider={authProvider}
+              dataProvider={dataProvider(
+                "https://api.fake-rest.refine.dev",
+                axiosInstance
+              )}
+              notificationProvider={useNotificationProvider}
+              resources={[
+                {
+                  name: "blog_posts",
+                  list: "/blog-posts",
+                  show: "/blog-posts/show/:id",
+                  create: "/blog-posts/create",
+                  edit: "/blog-posts/edit/:id",
+                  // this meta property enable delete button on both edit page and show page
+                  meta: {
+                    canDelete: true,
+                  },
+                },
+              ]}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+              }}
             >
-              <Route
-                index
-                element={<NavigateToResource resource="blog_posts" />}
-              />
-              <Route
-                path="blog-posts"
-                element={
-                  <Authenticated
-                    key="blog-posts"
-                    fallback={<CatchAllNavigate to="/login" />}
+              <Routes>
+                <Route>
+                  {/* <Route index element={<HomePage />} /> */}
+                  <Route index element={<HomePage />} />
+                </Route>
+                <Route
+                  element={
+                    <ThemedLayoutV2
+                      Header={ThemedHeaderV2}
+                      Sider={ThemedSiderV2}
+                      Title={ThemedTitleV2}
+                    >
+                      <Outlet />
+                    </ThemedLayoutV2>
+                  }
+                >
+                  <Route
+                    path="blog-posts"
+                    element={
+                      <Authenticated
+                        key="blog-posts"
+                        fallback={<CatchAllNavigate to="/login" />}
+                      >
+                        <Outlet />
+                      </Authenticated>
+                    }
                   >
-                    <Outlet />
-                  </Authenticated>
-                }
-              >
-                <Route index element={<BlogPostList />} />
-                <Route path="show/:id" element={<BlogPostShow />} />
-                <Route path="edit/:id" element={<BlogPostEdit />} />
-                <Route path="create" element={<BlogPostCreate />} />
-              </Route>
-            </Route>
-            <Route element={<Authenticated key="auth" fallback={<Outlet />} />}>
-              <Route path="/login" element={<AuthPage />} />
-              <Route path="/register" element={<AuthPage type="register" />} />
-              <Route
-                path="/forgot-password"
-                element={<AuthPage type="forgotPassword" />}
-              />
-            </Route>
-            <Route path="*" element={<ErrorComponent />} />
-          </Routes>
-          <UnsavedChangesNotifier />
-        </Refine>
+                    <Route index element={<BlogPostList />} />
+                    <Route path="show/:id" element={<BlogPostShow />} />
+                    <Route path="edit/:id" element={<BlogPostEdit />} />
+                    <Route path="create" element={<BlogPostCreate />} />
+                  </Route>
+                </Route>
+                <Route
+                  element={<Authenticated key="auth" fallback={<Outlet />} />}
+                >
+                  <Route path="/login" element={<AuthPage />} />
+                  <Route
+                    path="/register"
+                    element={<AuthPage type="register" />}
+                  />
+                  <Route
+                    path="/forgot-password"
+                    element={<AuthPage type="forgotPassword" />}
+                  />
+                </Route>
+                <Route path="*" element={<ErrorComponent />} />
+              </Routes>
+              <UnsavedChangesNotifier />
+            </Refine>
+          </DevtoolsProvider>
+        </AntdApp>
       </ConfigProvider>
     </BrowserRouter>
   );
